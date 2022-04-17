18e1b7afa6f3a723d7803b3cc17590f90027260b
arnold <67833474+luizarnoldch@users.noreply.github.com>
2022-04-17 17:19:06 -0500

    disco_entro_auth

---
+++ app/app.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+
+)
+
+func sanityCheck() {
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+	for _, k := range envProps {
+		if os.Getenv(k) == "" {
+			logger.Fatal(fmt.Sprintf("Environment variable %s not defined. Terminating application...", k))
+		}
+	}
+}
+
+
+func Start(){
+
+}
